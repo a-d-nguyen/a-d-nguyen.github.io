11cb3921ea31542d96d65a4a311387075ddfef43
Polarized <anthonyn@berkeley.edu>
2018-03-04 06:24:45 -0800

    first edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,12 @@
 $(document).ready(function() {
-	$('.homenav-item, .topnav-item, .port-category-box').hover(function() {
-    	$( this ).addClass( "black-hover" );
+	$('.page-name').hover(function() {
+    	$( this ).addClass( "hover" );
+    	$( '.page-name' ).not(".hover").addClass('fade');
   		}, function() {
-    		$( this ).removeClass( "black-hover" );
+    		$( this ).removeClass( "hover" );
+    		$( 'div' ).removeClass('fade');
   		}
 	);
-
-	$('.topnav-item').hover(function() {
-    	$( this ).addClass( "faded-hover" );
-  		}, function() {
-    		$( this ).removeClass( "faded-hover" );
-  		}
-	);
-
-	var slideIndex = 0;
-	carousel();
-
-	function carousel() {
-	    var i;
-	    var x = document.getElementsByClassName("mySlides");
-	    for (i = 0; i < x.length; i++) {
-	      x[i].style.display = "none"; 
-	    }
-	    slideIndex++;
-	    if (slideIndex > x.length) {slideIndex = 1} 
-	    x[slideIndex-1].style.display = "block"; 
-	    setTimeout(carousel, 3000); // Change image every 2.5 seconds
-	}
 
 })
 
